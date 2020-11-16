41354f1654be4c054412e32c4fd42b8c2e2bb53a
gamil91 <alexis_gamil99@yahoo.com.com>
2020-11-15 16:17:24 -0800

    commit tree a496b56bb59143b4f4a4d4493166cc3d6b288827

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, eventName){
+    let messagesArray = []
+    for(let i = 0; i < names.length; i ++){
+        messagesArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messagesArray;
+}
+
+function countDown(num){
+    let i = num
+    while (i >= 0){
+        console.log(i)
+        i -= 1;
+    }
+}
